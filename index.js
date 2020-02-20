407e11a8d7c98961e9de8009b738989b6c155aec
Michal Drzazga <drzamich@gmail.com>
2020-02-20 21:06:19 +0100

    smth working

--- index.js
+++ index.js
@@ -1,26 +1,73 @@
 const fs = require('fs');
 // const letters = ['a', 'b', 'c', 'd', 'e', 'f']
-const letters = ['a'];
+const letters = ['ab'];
 
 letters.forEach(letter => {
+
   fs.readFile(`input/${letter}.txt`, (err, data) => {
     const lines = data.toString().split('\n');
     const numberOfLibraries = lines[0].split(' ')[1];
-    const daysForScanning = lines[0].split(' ')[2];
+    let daysForScanning = lines[0].split(' ')[2];
     const books = lines[1].split(' ').map((score, index) => ({ id: index, score}))
     let libraries = [];
+
     for(let i = 0; i < numberOfLibraries; i++) {
       const totalBooks = lines[2*i+2].split(' ')[0];
       const signupDuration = lines[2*i+2].split(' ')[1];
       const shipCapacity = lines[2*i+2].split(' ')[2];
       const bookIds = lines[2*i+3].split(' ');
+      bookIds.sort((bookA, bookB) => {
+        return books[bookB].score - books[bookA].score
+      })
       libraries.push({ id: i, totalBooks, signupDuration, shipCapacity, bookIds });
     }
 
-    console.log('books: ');
-    console.log(books);
-    console.log('libraries: ');
-    console.log(libraries);
+    let librariesToLoad = [];
+
+    const chooseBestLibrary = (currentBest, _libraries) => {
+      let newLibraries = currentBest.booksToShip.length ? [..._libraries].filter(library => library.id !== currentBest.id) : [..._libraries];
+      daysForScanning -= currentBest.signupDuration;
+
+      newLibraries.forEach(library => {
+        library.bookIds = library.bookIds.filter(id => !currentBest.booksToShip.includes(id))
+        library.possibleShipments = Math.min((daysForScanning - library.signupDuration)*library.shipCapacity, library.bookIds.length);
+        library.booksToShip = library.bookIds.slice(0, library.possibleShipments);
+        let possibleScore = 0;
+        library.booksToShip.forEach(id => {
+          possibleScore += Number(books[id].score);
+        })
+        library.possibleScore = possibleScore;
+      });
+
+      let maxScore = 0;
+      let bestLibrary;
+      newLibraries.forEach(library => {
+        if(library.possibleScore > maxScore) {
+          bestLibrary = library;
+          maxScore = library.possibleScore;
+        }
+      });
+
+      librariesToLoad.push(bestLibrary);
+      if(newLibraries.length > 1) {
+        chooseBestLibrary(bestLibrary, newLibraries);
+      }
+    }
+
+    chooseBestLibrary({
+      daysForScanning: 0,
+      booksToShip: [],
+      signupDuration: 0,
+    }, libraries);
+
+    console.log(librariesToLoad);
+
+
+    // console.log(bestLibrary);
+    // console.log('books: ');
+    // console.log(books);
+    // console.log('libraries: ');
+    // console.log(libraries);
   });
 });
 
